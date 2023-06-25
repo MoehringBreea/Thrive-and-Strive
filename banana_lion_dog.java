798ad9b9bfdb87470d255b8c42614f42a0742bca
MoehringBreea <137548007+MoehringBreea@users.noreply.github.com>
2023-06-25 13:49:24 +0700

    Remove deprecated code

---
+++ banana_lion_dog.java
@@ -0,0 +1,80 @@
+//File name: 'FitnessWellnessCoachingProgram.java'
+
+public class FitnessWellnessCoachingProgram {
+
+	public static void main(String[] args){
+
+		// instantiate new FitnessWellnessCoaching object
+		FitnessWellnessCoaching fitnessWellnessCoaching = new FitnessWellnessCoaching();
+
+		// invoke client intake method
+		fitnessWellnessCoaching.clientIntake();
+
+		// invoke goal setting method
+		fitnessWellnessCoaching.goalSetting();
+
+		// invoke nutrition education method
+		fitnessWellnessCoaching.nutritionEducation();
+
+		// invoke exercise instruction method
+		fitnessWellnessCoaching.exerciseInstruction();
+
+		// invoke check in method
+		fitnessWellnessCoaching.checkIn();
+
+		// invoke goal attaining method
+		fitnessWellnessCoaching.goalAttaining();
+
+		// invoke ongoing relationship building method
+		fitnessWellnessCoaching.ongoingRelationshipBuilding();
+
+		// invoke celebrate successes method
+		fitnessWellnessCoaching.celebrateSuccesses();
+
+	}
+
+}
+
+class FitnessWellnessCoaching {
+
+	// method to conduct client intake
+	public void clientIntake(){
+		// code to gather and store client information
+	}
+
+	// method to set goals
+	public void goalSetting(){
+		// code to set realistic and achievable goals
+	}
+
+	// method to provide nutrition education
+	public void nutritionEducation(){
+		// code to provide nutrition education
+	}
+
+	// method to provide exercise instruction
+	public void exerciseInstruction(){
+		// code to provide exercise instruction
+	}
+
+	// method to check in
+	public void checkIn(){
+		// code to check in and assess progress
+	}
+
+	// method to attaining goals
+	public void goalAttaining(){
+		// code to celebrate reaching goals
+	}
+
+	// method to build ongoing relationships
+	public void ongoingRelationshipBuilding(){
+		// code to build ongoing relationships
+	}
+
+	// method to celebrate successes
+	public void celebrateSuccesses(){
+		// code to celebrate successes
+	}
+
+}
